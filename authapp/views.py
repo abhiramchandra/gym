bc5838bdc2a44deaee5eac6dcbccb56fd553b36b
abhiramchandra <abhiramchandra2002@gmail.com>
2024-08-13 23:09:40 -0700

    adding to repo

---
+++ authapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Home(request):
+    return render(request,"index.html")
+
+def signup(request):
+    return render(request,"signup.html")
